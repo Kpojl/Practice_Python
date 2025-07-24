c332f8e5297597d7ed48f63a97c912987e6acc9a
Andrey Zyryanov <paral1axcss@gmail.com>
2025-07-24 15:18:42 +0500

    task

--- First task/Week One/Week Two/Task_4.py
+++ First task/Week One/Week Two/Task_4.py
@@ -1,12 +1,14 @@
 data = []
-a = int(input("Введите целое число (0 для окончяния ввода): "))
 
-while a != 0:
-    data.append(a)
-    a = int(input("Введите целое число (0 для окончяния ввода): "))
+while True:
+    user_input = input("Введите целое число (0 для окончания ввода): ")
+    if user_input == "0":
+        break
+    try:
+        data.append(int(user_input))
+    except ValueError:
+        print("Ошибка! Введите целое число.")
 
 data.sort()
-
-print("Числа в порядке возрастания")
-for a in data:
-    print(a)
+print("\nЧисла в порядке возрастания:")
+print(*data, sep="\n")  # Красивый вывод
